53b5ccdfa7c0898e8aa8d1d60b228372c4e53b40
BetterNut <tejab2610@gmail.com>
2023-11-14 00:43:50 +0530

    Update CreateDiary.jsx

--- src/screens/diaries/CreateDiary.jsx
+++ src/screens/diaries/CreateDiary.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
+import Footer from '../components/footer';
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
