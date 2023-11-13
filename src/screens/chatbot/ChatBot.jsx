ac10c2cc6c5e68899b697d963e594c46c61b76f9
BetterNut <tejab2610@gmail.com>
2023-11-14 00:46:39 +0530

    Update ChatBot.jsx

--- src/screens/chatbot/ChatBot.jsx
+++ src/screens/chatbot/ChatBot.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from '../components/NavBar';
+import NavBar from '../components/Navbar';
 import Footer from '../components/footer';
 import ZapierEmbed from './embed';
 import Loading from './loading'; 
